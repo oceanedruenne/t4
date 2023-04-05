5efd7cbda150957e1de27970a9dd5fe600f1e5e9
Océane Druenne <oceane.druenne2@etu.unistra.fr>
2023-04-05 17:29:56 +0200

    ajout des questions d'evguenia et de céline

--- t4/src/routes/infos.jsx
+++ t4/src/routes/infos.jsx
@@ -2,43 +2,41 @@ import chat from '../assets/images/brouillon.png'
 import Jauge from '../routes/jauge';
 import person from '../data/person.json';
 
-function InfosPatient() {
-    return <div id="infos_patients">
-        <img src={chat} id="photo"></img>
-        <h2>{person.name}</h2>
-        <h3>Symptômes :</h3>
-        <ul>
-            {person.symptoms.map((symptom) => <li>{symptom}</li>)}
-        </ul>
-        <h3>Couverture : {person.cmu ? "oui" : "non"}</h3>
-        <h3>Pensées du patient : {person.thinking}</h3>
-    </div>
-}
-
-export default function Infos()
-{
-    return (
-        <div id="left_side">
-            <div id="infos_patients">
-            <img src={chat} id="photo"></img>
-                <h2>Julien Charistoy</h2>
-                <h3>Symptômes : Douleurs au coeur </h3>
-                <h3>Couverture : </h3>
-                <h3>Pensées du patient : </h3>
-                <div class="container">
-                    <div class="preloader">
-                        <span class="black"></span>
-                        <span class="black"></span>
-                        <span class="black"></span>
+export default function Infos() {
+    return  <div id="left_side">
+                <button 
+                    id="rules" 
+                    title="Vous êtes un médecin généraliste qui reçoit en consultation un patient, Julien Charistoy, qui se plaint de douleurs cardiaques. Votre but est de mener à bien cette consultation en faisant attention aux jauges du patient.">
+                    Règles du jeu
+                </button>
+                
+                <button id="informations" 
+                title="Cardiaque = coeur | Anxiolytique = Médicament qui aide à calmer l'anxieté, le stress d'une personne | Xanax = médicament de type anxiolytique | Angine de poitrine = Douleur vive ressentie au coeur lors d'un effort ou d'une forte émotion | ECG = ÉlectroCardioGramme | Méthylphénidate : Médicament qui aide à la concentration et qui peut augmenter le nombre de battements par minute du coeur | Testostérone : Hormone masculine">Lexique</button>
+                <div id="infos_patients">
+                    <img src={chat} id="photo"></img>
+                    <h2>{person.name}</h2>
+                    <h3>Symptômes :</h3>
+                    <ul>
+                        {person.symptoms.map((symptom) => <li>{symptom}</li>)}
+                    </ul>
+                    <h3>Couverture : {person.cmu ? "oui" : "non"}</h3>
+                    <h3>Pensées du patient : {person.thinking}</h3>
+                    <div class="container">
+                        <div class="preloader">
+                            <span class="black"></span>
+                            <span class="black"></span>
+                            <span class="black"></span>
+                        </div>
+                    </div>
+                    <div id="game_infos">
+                        <Jauge 
+                            defaultComprehension={person.comprehension}
+                            defaultInquietude={person.inquietude}
+                            defaultMotivation={person.motivation}
+                            defaultSastifaction={person.satisfaction}
+                        />
                     </div>
                 </div>
             </div>
-            <div id="game_infos">
-                <Jauge defaultComprehension={person.comprehension}
-                    defaultInquietude={person.inquietude}
-                    defaultMotivation={person.motivation}
-                    defaultSastifaction={person.satisfaction} />
-            </div>
-        </div>
-    )
 }
+
---
+++ t4/src/services/controller.jsx
@@ -0,0 +1,13 @@
+import person from "../data/person.json"
+import steps from "../data/steps.json"
+import jauge from "../routes/jauge"
+
+
+export default function Controller()
+{
+    // Faire en sorte de récupérer le tableau des options pour toutes les questions que l'on peut poser 
+    let first_question = document.getElementById("first_issues");
+    console.log(first_question);
+    // Récupérer les valeurs de chaque pensées
+    // Faire les opérations correspondantes
+}
