a6eda8247d2f0e84cb41e9200713bd1864421dc0
Charles LESECQ <charles.lesecq@etu.unistra.fr>
2023-04-05 14:01:39 +0200

    simplification json

--- t4/src/routes/chatbot.jsx
+++ t4/src/routes/chatbot.jsx
@@ -1,20 +1,11 @@
-import { useState, useEffect, useCallback } from 'react';
-import { Outlet, useParams } from 'react-router-dom';
 import ChatBot from "react-simple-chatbot";
+import steps from '../data/steps.json';
 
 export default function Infos()
 {
-    const [steps, setSteps] = useState([]);
-
-    useEffect(() => {
-        fetch('/steps.json')
-            .then(response => response.json())
-            .then(data => setSteps(data));
-    }, []);
-
     return (
         <div id="right_side" class="gradient-border">
-            { steps.length > 0 && <ChatBot steps={steps} /> }
+            { <ChatBot steps={steps} /> }
         </div>
     )
 }
